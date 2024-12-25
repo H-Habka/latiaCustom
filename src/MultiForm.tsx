5c23689ad11018f4e625f6d260caddbceca6a6b1
h-habke <hossien.habka1@gmail.com>
2024-12-25 09:56:31 +0400

    some changes

--- src/MultiForm.tsx
+++ src/MultiForm.tsx
@@ -1,94 +1,74 @@
 import { useState } from "react"
+import {
+  bangleSizeOptions,
+  chainLengthBraceletOptions,
+  chainLengthNecklaceOptions,
+  chainTypes,
+  itemTypes,
+  materials,
+  ringSizeOptions,
+  stones,
+  itemTypesT,
+  earringPushbackTypes,
+} from "./constants"
 
 const MultiStepForm = () => {
   const [step, setStep] = useState(1)
   const [formData, setFormData] = useState<{
     material: string
-    itemType: string
+    itemType: itemTypesT | null
     size: string
     chainLength: string
-    chainType: {
-      name?: string
-      imageURL?: string
-    }
+    chainType: string
     addStone: boolean
-    stoneType: {
-      name?: string
-      imageURL?: string
-    }
+    stoneType: string
     uploadedFile: string | null
     notes: string
+    earringPushbackType: string
   }>({
     material: "",
-    itemType: "",
+    itemType: null,
     size: "",
     chainLength: "",
-    chainType: {},
+    chainType: "",
     addStone: false,
-    stoneType: {},
+    stoneType: "",
     uploadedFile: null,
     notes: "",
+    earringPushbackType: "",
   })
+  const totalSteps = 6
+  const stepsActivationRules = [
+    formData.material,
+    formData.itemType &&
+      ((["Bangle", "Ring"].includes(formData.itemType) && formData.size) ||
+        (["Earring"].includes(formData.itemType) &&
+          formData.earringPushbackType) ||
+        (["Necklace", "Bracelet", "Anklet"].includes(formData.itemType) &&
+          formData.chainLength &&
+          formData.chainType) ||
+        ["Pendant"].includes(formData.itemType)),
+  ]
   const [preview, setPreview] = useState<string>("")
 
-  const materials = [
-    "18k Yellow Gold",
-    "18k Rose Gold",
-    "18k White Gold",
-    "21k Yellow Gold",
-    "Silver",
-  ]
-
-  const itemTypes = [
-    "Necklace",
-    "Earring",
-    "Ring",
-    "Bracelet",
-    "Bangle",
-    "Pendant",
-    "Anklet",
-  ]
-
-  const stones = [
-    {
-      name: "Malachite",
-      imageURL: "https://i.ebayimg.com/images/g/7z4AAOSw~uxkRsZA/s-l1200.jpg",
-    },
-    {
-      name: "White MOP",
-      imageURL: "https://i.ebayimg.com/images/g/7z4AAOSw~uxkRsZA/s-l1200.jpg",
-    },
-    {
-      name: "Gray MOP",
-      imageURL: "https://i.ebayimg.com/images/g/7z4AAOSw~uxkRsZA/s-l1200.jpg",
-    },
-    {
-      name: "Lapiz",
-      imageURL: "https://i.ebayimg.com/images/g/7z4AAOSw~uxkRsZA/s-l1200.jpg",
-    },
-  ]
-
-  const chainTypes = [
-    {
-      name: "Type 1",
-      imageURL: "https://i.ebayimg.com/images/g/7z4AAOSw~uxkRsZA/s-l1200.jpg",
-    },
-    {
-      name: "Type 2",
-      imageURL: "https://i.ebayimg.com/images/g/7z4AAOSw~uxkRsZA/s-l1200.jpg",
-    },
-    {
-      name: "Type 3",
-      imageURL: "https://i.ebayimg.com/images/g/7z4AAOSw~uxkRsZA/s-l1200.jpg",
-    },
-    {
-      name: "Type 4",
-      imageURL: "https://i.ebayimg.com/images/g/7z4AAOSw~uxkRsZA/s-l1200.jpg",
-    },
-  ]
-
   const handleNext = () => {
-    if (step < 6) setStep(step + 1)
+    if (step < totalSteps) setStep(step + 1)
+    if (step === totalSteps) {
+      setStep(1)
+      setPreview("")
+      setFormData({
+        material: "",
+        itemType: null,
+        size: "",
+        chainLength: "",
+        chainType: "",
+        addStone: false,
+        stoneType: "",
+        uploadedFile: null,
+        notes: "",
+        earringPushbackType: "",
+      })
+    }
   }
 
   const handleBack = () => {
@@ -101,30 +81,13 @@ const MultiStepForm = () => {
     setPreview(URL.createObjectURL(file))
   }
 
-  //   const handleSubmit = () => {
-  //     alert(`
-  //       Your custom piece:
-  //       Material: ${formData.material}
-  //       Item Type: ${formData.itemType}
-  //       Size: ${formData.size || "Not specified"}
-  //       Chain Length: ${formData.chainLength || "Not specified"}
-  //       Chain Type: ${formData.chainType.name || "Not specified"}
-  //       Add Stone: ${
-  //         formData.addStone ? `Yes (${formData.stoneType.name})` : "No"
-  //       }
-  //       Stone Type: ${formData.stoneType.name || "Not selected"}
-  //       Uploaded File: ${formData.uploadedFile?.name || "No file uploaded"}
-  //       Notes: ${formData.notes || "No special requests"}
-  //     `)
-  //   }
-
   const renderStep = () => {
     switch (step) {
       case 1:
         return (
           <div className="w-full max-w-screen-xl mx-auto px-2 ">
             <h2 className="text-2xl font-semibold mb-6">
-              Step 1: Choose Material
+              Step {step}: Choose Material
             </h2>
             <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 md:gap-6 gap-4">
               {materials.map((material, index) => (
@@ -133,7 +96,7 @@ const MultiStepForm = () => {
                   className={`py-3 px-6 border rounded-lg text-center ${
                     formData.material === material
                       ? "bg-blue-500 text-white"
-                      : "bg-gray-100 hover:bg-gray-200"
+                      : "bg-gray-200 hover:bg-gray-200"
                   }`}
                   onClick={() => setFormData({ ...formData, material })}
                 >
@@ -147,7 +110,7 @@ const MultiStepForm = () => {
         return (
           <div className="w-full max-w-screen-xl mx-auto  px-2">
             <h2 className="text-2xl font-semibold mb-6">
-              Step 2: Choose Item Type
+              Step {step}: Choose Item Type
             </h2>
             <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 md:gap-6 gap-4">
               {itemTypes.map((type, index) => (
@@ -156,7 +119,7 @@ const MultiStepForm = () => {
                   className={`py-3 px-6 border rounded-lg text-center ${
                     formData.itemType === type
                       ? "bg-blue-500 text-white"
-                      : "bg-gray-100 hover:bg-gray-200"
+                      : "bg-gray-200 hover:bg-gray-200"
                   }`}
                   onClick={() =>
                     setFormData({
@@ -164,7 +127,8 @@ const MultiStepForm = () => {
                       itemType: type,
                       size: "",
                       chainLength: "",
-                      chainType: {},
+                      chainType: "",
+                      earringPushbackType: "",
                     })
                   }
                 >
@@ -176,56 +140,99 @@ const MultiStepForm = () => {
               formData.itemType === "Bangle") && (
               <div className="mt-4">
                 <label className="block text-gray-700">Enter Size:</label>
-                <input
-                  type="text"
+                <select
                   className="w-full mt-2 p-3 border rounded-lg"
-                  placeholder="Size (e.g., 6, 7, etc.)"
                   value={formData.size}
                   onChange={(e) =>
                     setFormData({ ...formData, size: e.target.value })
                   }
-                />
+                >
+                  <option value="" disabled>
+                    Select Length (e.g., 16, 18 inches)
+                  </option>
+                  {(formData.itemType === "Ring"
+                    ? ringSizeOptions
+                    : bangleSizeOptions
+                  ).map((option) => (
+                    <option key={option.value} value={option.value}>
+                      {option.label}
+                    </option>
+                  ))}
+                </select>
               </div>
             )}
             {(formData.itemType === "Necklace" ||
               formData.itemType === "Bracelet") && (
               <div className="mt-4">
                 <label className="block text-gray-700">Chain Length:</label>
-                <input
-                  type="text"
+                <select
                   className="w-full mt-2 p-3 border rounded-lg"
-                  placeholder="Length (e.g., 16, 18 inches)"
                   value={formData.chainLength}
                   onChange={(e) =>
                     setFormData({ ...formData, chainLength: e.target.value })
                   }
-                />
+                >
+                  <option value="" disabled>
+                    Select Length (e.g., 16, 18 inches)
+                  </option>
+                  {(formData.itemType === "Necklace"
+                    ? chainLengthNecklaceOptions
+                    : chainLengthBraceletOptions
+                  ).map((option) => (
+                    <option key={option.value} value={option.value}>
+                      {option.label}
+                    </option>
+                  ))}
+                </select>
                 <label className="block text-gray-700 mt-4 mb-2">
                   Chain Type:
                 </label>
                 <div className="grid grid-cols-2 sm:grid-cols-3 gap-4">
                   {chainTypes.map((type, index) => (
-                    <div>
+                    <div key={index}>
                       <button
                         style={{
                           backgroundImage: `url('${type.imageURL}')`,
                         }}
-                        key={index}
-                        className={`w-full bg-cover bg-no-repeat min-h-[200px] py-3 px-6  rounded-lg border-2 text-center ${
-                          formData.chainType.name === type.name
+                        className={`w-full bg-cover bg-no-repeat aspect-square py-3 px-6  rounded-lg border-2 text-center ${
+                          formData.chainType === type.name
                             ? "border-blue-500 text-white"
                             : "border-gray-100 hover:bg-gray-200"
                         }`}
                         onClick={() =>
-                          setFormData({ ...formData, chainType: type })
+                          setFormData({ ...formData, chainType: type.name })
                         }
-                      >
-                        {/* <img
-                      src={stone.imageURL}
-                      alt={stone.name}
-                      className="w-12 h-12 mx-auto"
-                    /> */}
-                      </button>
+                      />
+                      <p className="text-center mt-2">{type.name}</p>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            )}
+            {formData.itemType === "Earring" && (
+              <div className="mt-4">
+                <label className="block text-gray-700 mt-4 mb-2">
+                  Pushback Type:
+                </label>
+                <div className="grid grid-cols-2 sm:grid-cols-3 gap-4">
+                  {earringPushbackTypes.map((type, index) => (
+                    <div key={index}>
+                      <button
+                        style={{
+                          backgroundImage: `url('${type.imageURL}')`,
+                        }}
+                        className={`w-full bg-cover bg-no-repeat aspect-square py-3 px-6  rounded-lg border-2 text-center ${
+                          formData.earringPushbackType === type.name
+                            ? "border-blue-500 text-white"
+                            : "border-gray-100 hover:bg-gray-200"
+                        }`}
+                        onClick={() =>
+                          setFormData({
+                            ...formData,
+                            earringPushbackType: type.name,
+                          })
+                        }
+                      />
                       <p className="text-center mt-2">{type.name}</p>
                     </div>
                   ))}
@@ -241,43 +248,45 @@ const MultiStepForm = () => {
               Step 3: Add a Stone (Optional)
             </h2>
             <div className="flex items-center mb-4">
-              <label className="mr-2">Add a Stone?</label>
-              <input
-                type="checkbox"
-                checked={formData.addStone}
-                onChange={(e) =>
+              <label className="mr-4 text-gray-700 font-medium">
+                Add a Stone?
+              </label>
+              <button
+                onClick={() =>
                   setFormData({
                     ...formData,
-                    addStone: e.target.checked,
-                    stoneType: {},
+                    addStone: !formData.addStone,
+                    stoneType: "",
                   })
                 }
-              />
+                className={`relative inline-flex h-6 w-11 items-center rounded-full transition-colors ${
+                  formData.addStone ? "bg-blue-500" : "bg-gray-300"
+                }`}
+              >
+                <span
+                  className={`inline-block h-4 w-4 transform rounded-full bg-white transition-transform ${
+                    formData.addStone ? "translate-x-6" : "translate-x-1"
+                  }`}
+                ></span>
+              </button>
             </div>
             {formData.addStone && (
               <div className="grid grid-cols-2 sm:grid-cols-3 gap-4">
                 {stones.map((stone, index) => (
-                  <div>
+                  <div key={index}>
                     <button
                       style={{
                         backgroundImage: `url('${stone.imageURL}')`,
                       }}
-                      key={index}
-                      className={`w-full bg-cover bg-no-repeat min-h-[200px] py-3 px-6  rounded-lg border-2 text-center ${
-                        formData.stoneType.name === stone.name
+                      className={`w-full bg-cover aspect-square bg-no-repeat py-3 px-6  rounded-lg border-2 text-center ${
+                        formData.stoneType === stone.name
                           ? "border-blue-500 text-white"
                           : "border-gray-100 hover:bg-gray-200"
                       }`}
                       onClick={() =>
-                        setFormData({ ...formData, stoneType: stone })
+                        setFormData({ ...formData, stoneType: stone.name })
                       }
-                    >
-                      {/* <img
-                      src={stone.imageURL}
-                      alt={stone.name}
-                      className="w-12 h-12 mx-auto"
-                    /> */}
-                    </button>
+                    />
                     <p className="text-center mt-2">{stone.name}</p>
                   </div>
                 ))}
@@ -304,7 +313,7 @@ const MultiStepForm = () => {
                 <img
                   src={preview}
                   alt="Preview"
-                  className="w-[320px] h-[320px] mb-4"
+                  className="w-[320px] h-[320px] mt-4"
                 />
               )}
             </div>
@@ -332,11 +341,11 @@ const MultiStepForm = () => {
               </p>
               <p>
                 <strong>Chain Type:</strong>{" "}
-                {formData.chainType.name || "Not selected"}
+                {formData.chainType || "Not selected"}
               </p>
               <p>
                 <strong>Stone Type:</strong>{" "}
-                {formData.stoneType.name || "Not selected"}
+                {formData.stoneType || "Not selected"}
               </p>
               {formData.uploadedFile && (
                 <div>
@@ -358,9 +367,6 @@ const MultiStepForm = () => {
       case 6:
         return (
           <div className="w-full max-w-screen-xl mx-auto  px-2">
-            <h2 className="text-2xl font-semibold mb-6">
-              Step 6: Submit Order
-            </h2>
             <p>Your order has been successfully submitted!</p>
           </div>
         )
@@ -370,26 +376,36 @@ const MultiStepForm = () => {
   }
 
   return (
-    <div className="h-screen bg-gray-100">
-      <div className="fixed w-full start-0 top-0 flex items-center bg-[#A8A8A8] py-8 px-4">
-        <p className="text-5xl text-[#EAEAEA]">LOGO</p>
+    <div className="h-screen bg-gray-200">
+      <div className="fixed w-full start-0 top-0 flex items-center bg-white py-2 px-4 shadow-sm  justify-center z-[10]">
+        <img src="/latia-logo.png" alt="logo" className="w-[170px] " />
       </div>
-      <div className="w-full flex flex-col bg-white shadow-lg pt-[120px] pb-12 min-h-full">
+      <div className="w-full flex flex-col bg-gray-50 shadow-lg pt-[120px] pb-20 min-h-full">
         <div className="flex-grow h-full p-2 md:p-8">{renderStep()}</div>
         <div className=" fixed bottom-0 start-0  w-full bg-gray-200 p-4 flex justify-between">
-          <button
-            className="bg-gray-400 text-white px-6 py-3 rounded-lg"
-            onClick={handleBack}
-            disabled={step === 1}
-          >
-            Back
-          </button>
+          {step !== totalSteps ? (
+            <button
+              className={` text-white px-6 py-3 rounded-lg ${
+                step === totalSteps ? "bg-blue-500" : "bg-gray-400"
+              }`}
+              onClick={handleBack}
+              disabled={step === 1}
+            >
+              Back
+            </button>
+          ) : (
+            <div></div>
+          )}
           <button
             className="bg-blue-500 text-white px-6 py-3 rounded-lg disabled:bg-gray-300"
             onClick={handleNext}
-            disabled={step === 6}
+            disabled={!stepsActivationRules[step - 1]}
           >
-            Next
+            {step === totalSteps - 1
+              ? "Submit"
+              : step === totalSteps
+              ? "Create Another Order"
+              : "Next"}
           </button>
         </div>
       </div>
---
+++ src/constants/index.ts
@@ -0,0 +1,170 @@
+export const materials = [
+  "18k Yellow Gold",
+  "18k Rose Gold",
+  "18k White Gold",
+  "21k Yellow Gold",
+  "Silver",
+]
+
+export type itemTypesT =
+  | "Necklace"
+  | "Earring"
+  | "Ring"
+  | "Bracelet"
+  | "Bangle"
+  | "Pendant"
+  | "Anklet"
+
+export const itemTypes: itemTypesT[] = [
+  "Necklace",
+  "Earring",
+  "Ring",
+  "Bracelet",
+  "Bangle",
+  "Pendant",
+  "Anklet",
+]
+
+export const chainLengthNecklaceOptions = [
+  {
+    label: "1",
+    value: "1",
+  },
+  {
+    label: "2",
+    value: "2",
+  },
+  {
+    label: "3",
+    value: "3",
+  },
+  {
+    label: "4",
+    value: "4",
+  },
+  {
+    label: "5",
+    value: "5",
+  },
+]
+
+export type earringPushbackTypeT = {
+  name: "heart" | "butterfly"
+  imageURL: string
+}
+
+export const earringPushbackTypes: earringPushbackTypeT[] = [
+  {
+    name: "heart",
+    imageURL: "https://i.ebayimg.com/images/g/7z4AAOSw~uxkRsZA/s-l1200.jpg",
+  },
+  {
+    name: "butterfly",
+    imageURL: "https://i.ebayimg.com/images/g/7z4AAOSw~uxkRsZA/s-l1200.jpg",
+  },
+]
+export const ringSizeOptions = [
+  {
+    label: "1",
+    value: "1",
+  },
+  {
+    label: "2",
+    value: "2",
+  },
+  {
+    label: "3",
+    value: "3",
+  },
+  {
+    label: "4",
+    value: "4",
+  },
+  {
+    label: "5",
+    value: "5",
+  },
+]
+export const bangleSizeOptions = [
+  {
+    label: "1",
+    value: "1",
+  },
+  {
+    label: "2",
+    value: "2",
+  },
+  {
+    label: "3",
+    value: "3",
+  },
+  {
+    label: "4",
+    value: "4",
+  },
+  {
+    label: "5",
+    value: "5",
+  },
+]
+
+export const chainLengthBraceletOptions = [
+  {
+    label: "10",
+    value: "10",
+  },
+  {
+    label: "20",
+    value: "20",
+  },
+  {
+    label: "30",
+    value: "30",
+  },
+  {
+    label: "40",
+    value: "40",
+  },
+  {
+    label: "50",
+    value: "50",
+  },
+]
+
+export const stones = [
+  {
+    name: "Malachite",
+    imageURL: "https://i.ebayimg.com/images/g/7z4AAOSw~uxkRsZA/s-l1200.jpg",
+  },
+  {
+    name: "White MOP",
+    imageURL: "https://i.ebayimg.com/images/g/7z4AAOSw~uxkRsZA/s-l1200.jpg",
+  },
+  {
+    name: "Gray MOP",
+    imageURL: "https://i.ebayimg.com/images/g/7z4AAOSw~uxkRsZA/s-l1200.jpg",
+  },
+  {
+    name: "Lapiz",
+    imageURL: "https://i.ebayimg.com/images/g/7z4AAOSw~uxkRsZA/s-l1200.jpg",
+  },
+]
+
+export const chainTypes = [
+  {
+    name: "Type 1",
+    imageURL: "https://i.ebayimg.com/images/g/7z4AAOSw~uxkRsZA/s-l1200.jpg",
+  },
+  {
+    name: "Type 2",
+    imageURL: "https://i.ebayimg.com/images/g/7z4AAOSw~uxkRsZA/s-l1200.jpg",
+  },
+  {
+    name: "Type 3",
+    imageURL: "https://i.ebayimg.com/images/g/7z4AAOSw~uxkRsZA/s-l1200.jpg",
+  },
+  {
+    name: "Type 4",
+    imageURL: "https://i.ebayimg.com/images/g/7z4AAOSw~uxkRsZA/s-l1200.jpg",
+  },
+]
